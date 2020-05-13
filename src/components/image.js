652cad1a42470bfffcef61b3976e0ff4e570a41c
Mike Jenkins <kinetic@icloud.com>
2020-05-13 16:26:36 -0400

    Added filter to image component.

--- src/components/image.js
+++ src/components/image.js
@@ -8,7 +8,7 @@ const Image = props => (
   <StaticQuery
     query={graphql`
       query {
-        images: allFile {
+        images: allFile(filter:{ extension: { regex: "/jpeg|jpg|png|gif/"}}) {
           edges {
             node {
               relativePath
