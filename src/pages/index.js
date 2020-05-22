6c0376b4d2a72471c8682eb3c73fd85e2ebbfdcb
Mike Jenkins <kinetic@icloud.com>
2020-05-22 12:50:44 -0400

    Added Cypress Basic Config

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import SEO from "../components/seo"
 import { Link } from "gatsby"
 
 const IndexPage = () => (
-  <div>
+  <main id="main-content" role="main">
       <SEO title="Language Select" />
       <Link to="/en">English</Link>
       <Link to="/fr">François</Link>
-  </div>
+  </main>
 )
 
 export default IndexPage
