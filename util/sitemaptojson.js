01174232313015e0f3d26226756cc0eedd8df912
Mike Jenkins <kinetic@icloud.com>
2020-06-15 12:03:18 -0400

    Build utility to convert xml sitemap to json.

---
+++ util/sitemaptojson.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const DomParser = require('dom-parser');
+const parser = new DomParser();
+
+const processXML = (string) => {
+    let parsed = parser.parseFromString(string, "text/xml")
+    let urlArray = parsed.getElementsByTagName('loc')
+    let result = urlArray.map(node => {
+        return node.firstChild.text.replace('https://travel-guidebook.herokuapp.com', '')
+    });
+    return result;
+}
+
+fs.readFile('././sitemap.xml', 'utf8', function(err, data) {
+    if (err) throw err;
+    let result = processXML(data)
+    let json = JSON.stringify(result);
+    fs.writeFile('././sitemap.json', json, (err) => {
+        if (err) throw err;
+        console.log('JSON sitemap saved.');
+    });
+});
