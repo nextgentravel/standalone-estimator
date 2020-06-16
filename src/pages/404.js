b5ffef124dbf3807700c9c177b66d9bc565ed7dd
Mike Jenkins <kinetic@icloud.com>
2020-06-16 10:20:09 -0400

    Added triangle warning icons

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,18 +4,20 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 
+import { FaExclamationTriangle } from 'react-icons/fa';
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
       <div className="container css404">
         <div className="row">
           <div className="col-lg-6 col-md-12 css404" lang="fr">
-            <h2>Nous ne pouvons trouver cette page (Erreur 404)</h2>
+            <h2><FaExclamationTriangle /> Nous ne pouvons trouver cette page (Erreur 404)</h2>
             <p>Nous sommes désolés que vous ayez abouti ici. Il arrive parfois qu'une page ait été déplacée ou supprimée. Heureusement, nous pouvons vous aider à trouver ce que vous cherchez.</p>
             <Link to="/fr">Retournez à la page d'accueil</Link>
           </div>
           <div className="col-lg-6 col-md-12 css404" lang="en">
-            <h2>We couldn't find that page (Error 404)</h2>
+            <h2><FaExclamationTriangle /> We couldn't find that page (Error 404)</h2>
             <p>We're sorry you ended up here. Sometimes a page gets moved or deleted, but hopefully we can help you find what you're looking for.</p>
             <Link to="/en">Return to the home page</Link>
           </div>
