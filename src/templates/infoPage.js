dfbedb1c2ff85c9bfc898faf992793deb57d0dd2
Mike Jenkins <kinetic@icloud.com>
2020-06-17 13:37:04 -0400

    Added breadcrumbs to template

--- src/templates/infoPage.js
+++ src/templates/infoPage.js
@@ -6,6 +6,14 @@ export default ({ data }) => {
   return (
     <Layout>
         <div className="info-page-container container">
+          <div class="container pl-0">
+              <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb pl-0">
+                      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">Suggest an Update</li>
+                  </ol>
+              </nav>
+          </div>
           <div className="info-page row">
               <main
               className="info-page-content col-12"
