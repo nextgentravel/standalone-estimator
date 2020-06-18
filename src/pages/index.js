9c2ab9ef14a56d2d765b3cc014fb87331353c7b0
Mike Jenkins <kinetic@icloud.com>
2020-06-18 15:27:15 -0400

    media query on top margin of card, styling changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,29 @@
 import React from "react"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
+import { FaCircle } from 'react-icons/fa';
 
 const IndexPage = () => (
   <main id="main-content" role="main">
     <div id="bg"></div>
-      <SEO title="Language Select" />
-      <div className="card lang">
-        <div className="card-body">
-          <image></image>
-          <Link to="/fr" className="langlink btn btn-primary">Français</Link>
-          <Link to="/en" className="langlink btn btn-primary">English</Link>
+    <SEO title="Travel Guide - Language Select" />
+    <div className="card lang">
+      <div className="card-body d-flex justify-content-center">
+        <image></image>
+        <Link to="/fr" className="btn btn-primary m-2 p-2">Français</Link>
+        <Link to="/en" className="btn btn-primary m-2 p-2">English</Link>
+      </div>
+      <div className="card-body bg-dark">
+        <div className="col-6 float-left">
+          <a class="text-light" href="">Avis</a>
+          &nbsp;<FaCircle size={5} class="text-secondary" />&nbsp;
+          <a class="text-light" href="">Terms and conditions</a>
         </div>
-        <div className="card-body splash">
-          <div className="col-6">
-            <p><a href="https://www.canada.ca/fr/transparence/avis.html">Avis</a>
-            <span id="divider">●</span>
-            <a href="https://www.canada.ca/en/transparency/terms.html">Terms and conditions</a></p> 
-          </div>
-          <div className="col-6">
-            <image></image>
-          </div>
+        <div className="col-6">
+          <image></image>
         </div>
       </div>
+    </div>
   </main>
 )
 export default IndexPage
