da1e7151bf2131d84abecc3d75a4ec42193c51a1
Mike Jenkins <kinetic@icloud.com>
2020-06-23 15:49:01 -0400

    Loading resuable component for less duplicative code.

--- src/pages/rates.en.js
+++ src/pages/rates.en.js
@@ -1,33 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import InputDatalist from "../components/input-datalist.js"
+import RatesChecker from "../components/rates-checker"
 
-// the following will be fetched from an API
-let mockCityList = [
-  {
-      label: "Barrie",
-      value: "Barrie"
-  },
-  {
-      label: "Calgary",
-      value: "Calgary"
-  },
-]
+const RatesPage = () => {
+  return (
+    <Layout>
+      <SEO title="Rates and Limits" />
+      <w-screen mt-4="true" fluid="true" id="container">
+        <div>
+          <main id="main-content" role="main">
+            <div className="container mt-4">
+              <RatesChecker />
+            </div>
+          </main>
+        </div>
+      </w-screen>
+    </Layout>
+  )
+}
 
-const Rates = () => (
-  <Layout>
-    <SEO title="Rates and Limits" />
-    <w-screen mt-4="true" fluid="true" id="container">
-      <div>
-        <main id="main-content" role="main">
-          <div className="container mt-4">
-            <InputDatalist label="City" name="city" options={mockCityList} />
-          </div>
-        </main>
-      </div>
-    </w-screen>
-  </Layout>
-)
-
-export default Rates;
+export default RatesPage;
