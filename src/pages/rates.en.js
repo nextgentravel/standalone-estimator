1c4b1e51abdb95943c4b4469c487214b75afd094
Mike Jenkins <kinetic@icloud.com>
2020-06-23 14:15:36 -0400

    Removed unused imports

--- src/pages/rates.en.js
+++ src/pages/rates.en.js
@@ -1,21 +1,33 @@
 import React from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import { Link } from "gatsby"
+import InputDatalist from "../components/input-datalist.js"
+
+// the following will be fetched from an API
+let mockCityList = [
+  {
+      label: "Barrie",
+      value: "Barrie"
+  },
+  {
+      label: "Calgary",
+      value: "Calgary"
+  },
+]
 
 const Rates = () => (
   <Layout>
-  <SEO title="Rates and Limits" />
-  <w-screen mt-4="true" fluid="true" id="container">
-    <div>
-      <main id="main-content" role="main">
-        <div className="container mt-4">
-        </div>
-      </main>
-    </div>
-  </w-screen>
-</Layout>
+    <SEO title="Rates and Limits" />
+    <w-screen mt-4="true" fluid="true" id="container">
+      <div>
+        <main id="main-content" role="main">
+          <div className="container mt-4">
+            <InputDatalist label="City" options={mockCityList} />
+          </div>
+        </main>
+      </div>
+    </w-screen>
+  </Layout>
 )
 
 export default Rates;
--- src/pages/rates.fr.js
+++ src/pages/rates.fr.js
@@ -1,8 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import { Link } from "gatsby"
 
 const Rates = () => (
   <Layout>
