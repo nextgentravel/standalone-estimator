b68865f5b3071f15a8ac5f3941bb2cab4890da2d
Mike Jenkins <kinetic@icloud.com>
2020-06-23 11:41:52 -0400

    Added new fr and en pages for rates.

---
+++ src/pages/rates.en.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+const Rates = () => (
+  <Layout>
+  <SEO title="Rates and Limits" />
+  <w-screen mt-4="true" fluid="true" id="container">
+    <div>
+      <main id="main-content" role="main">
+        <div className="container mt-4">
+        </div>
+      </main>
+    </div>
+  </w-screen>
+</Layout>
+)
+
+export default Rates;
