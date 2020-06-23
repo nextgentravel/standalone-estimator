5b19b00d1b1aec5b6304a1a98f4fecb461e92c44
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-06-23 16:36:49 -0300

    date picker added

--- src/pages/rates.en.js
+++ src/pages/rates.en.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import InputDatalist from "../components/input-datalist.js"
+import DatePicker from "../components/date-picker"
 
 // the following will be fetched from an API
 let mockCityList = [
@@ -23,6 +24,12 @@ const Rates = () => (
         <main id="main-content" role="main">
           <div className="container mt-4">
             <InputDatalist label="City" name="city" options={mockCityList} />
+          </div>
+          <div className="container mt-4">
+            <DatePicker label="Start Date" name="start"></DatePicker>
+          </div>
+          <div className="container mt-4">
+            <DatePicker label="End Date" name="end"></DatePicker>
           </div>
         </main>
       </div>
