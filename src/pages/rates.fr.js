4b01268fb39cef3cbdf8a10b9d64001288139686
Mike Jenkins <kinetic@icloud.com>
2020-06-24 15:21:42 -0400

    Added breadcrumbs

--- src/pages/rates.fr.js
+++ src/pages/rates.fr.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Breadcrumbs from "../components/breadcrumb"
 
 const Rates = () => (
   <Layout>
@@ -8,7 +9,9 @@ const Rates = () => (
   <w-screen mt-4="true" fluid="true" id="container">
     <div>
       <main id="main-content" role="main">
+        <Breadcrumbs pageTitle={'Trouvez vos tarifs et limites'} homeLink={'/fr/'} />
         <div className="container mt-4">
+          <p>Developing in English, for now.</p>
         </div>
       </main>
     </div>
