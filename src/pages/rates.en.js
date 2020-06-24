61914d114dc9a29b012dd5f60ecf7cde5b021f8f
Mike Jenkins <kinetic@icloud.com>
2020-06-24 15:21:42 -0400

    Added breadcrumbs

--- src/pages/rates.en.js
+++ src/pages/rates.en.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import RatesChecker from "../components/rates-checker"
+import Breadcrumbs from "../components/breadcrumb"
 
 const RatesPage = () => {
   return (
@@ -10,6 +11,7 @@ const RatesPage = () => {
       <w-screen mt-4="true" fluid="true" id="container">
         <div>
           <main id="main-content" role="main">
+            <Breadcrumbs pageTitle={'Find Your Rates and Limits'} homeLink={'/en/'} />
             <div className="container mt-4">
               <RatesChecker />
             </div>
