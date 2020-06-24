647a92ca243ddebc7809c3d0040928283172c7b2
Mike Jenkins <kinetic@icloud.com>
2020-06-24 15:22:10 -0400

    Tidied up css

--- src/components/input-datalist.js
+++ src/components/input-datalist.js
@@ -3,9 +3,9 @@ import React from "react"
 const InputDatalist = ({label, name, options, updateValue}) => {
 
     return (
-        <div>
+        <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input type="text" id={name} name={name} list="suggestions" onChange={event => updateValue(event.target.value)} />
+            <input className="col-12" type="text" id={name} name={name} list="suggestions" onChange={event => updateValue(event.target.value)} />
             <datalist id="suggestions">
                 {/* TODO <!--[if lte IE 9]><select><![endif]--> */}
                 {options.map((option, index) =>
