91179a27656b76262d369617ecc137a4de1695b4
Mike Jenkins <kinetic@icloud.com>
2020-06-25 15:02:38 -0400

    Removed unnecessary dependencies and fixed classes.

--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -5,7 +5,7 @@ const DatePicker = ({label, name, updateValue}) => {
     return (
         <div className="mb-4">
 	        <label htmlFor={name}>{label}</label>
-	        <input class="form-control" type="date" id={name} name={name} onChange={event => updateValue(event.target.value)}/>
+	        <input className="form-control" type="date" id={name} name={name} onChange={event => updateValue(event.target.value)}/>
         </div>
     )
 }
