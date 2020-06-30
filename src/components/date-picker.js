1c57ae8cdf19708af4213eab61cb1963b35de9ab
Mike Jenkins <kinetic@icloud.com>
2020-06-30 10:47:26 -0400

    Added validation logic to inputs

--- src/components/date-picker.js
+++ src/components/date-picker.js
@@ -1,11 +1,39 @@
 import React from "react"
 
-const DatePicker = ({label, name, updateValue}) => {
+const DatePicker = ({validationWarnings, setValidationWarnings, label, name, updateValue}) => {
+    let showValidationWarning = false;
+    let componentWarnings = []
+    validationWarnings.forEach(warning => {
+        if (warning.path === name) {
+            componentWarnings.push(warning);
+        }
+    })
+
+    if (componentWarnings.length > 0) {
+        showValidationWarning = true;
+    }
+
+    const handleRemoveError = name => {
+        setValidationWarnings(validationWarnings.filter(item => item.path !== name))
+    }
 
     return (
         <div className="mb-4">
 	        <label htmlFor={name}>{label}</label>
-	        <input className="form-control" type="date" id={name} name={name} onChange={event => updateValue(event.target.value)}/>
+            <input
+                aria-describedby={`${name}-error`}
+                className={showValidationWarning ? 'form-control is-invalid' : 'form-control' }
+                type="date"
+                id={name}
+                name={name}
+                onChange={event => {
+                    handleRemoveError(name)
+                    updateValue(event.target.value)
+                }}
+            />
+            {componentWarnings.map((warning, index) => (
+                <small key={index} id={`${name}-error`} className="invalid-feedback">{warning.message}</small>
+            ))}
         </div>
     )
 }
