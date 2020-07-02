17a5737a322211cc0e40f023c9f0a7b7f77167be
Mike Jenkins <kinetic@icloud.com>
2020-07-02 15:13:31 -0400

    These aren't labels yet.

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,54 +1,54 @@
 module.exports = {
     AB: {
         label: "Alberta",
-        value: "45.5"
+        rateCents: "45.5"
     },
     BC: {
         label: "British Columbia",
-        value: "51.0"
+        rateCents: "51.0"
     },
     MB: {
         label: "Manitoba",
-        value: "48.0"
+        rateCents: "48.0"
     },
     NB: {
         label: "New Brunswick",
-        value: "50.0"
+        rateCents: "50.0"
     },
     NL: {
         label: "Newfoundland and Labrador",
-        value: "53.5"
+        rateCents: "53.5"
     },
     NT: {
         label: "Northwest Territories",
-        value: "59.0"
+        rateCents: "59.0"
     },
     NS: {
         label: "Nova Scotia",
-        value: "50.0"
+        rateCents: "50.0"
     },
     NU: {
         label: "Nunavut",
-        value: "59.0"
+        rateCents: "59.0"
     },
     ON: {
         label: "Ontario",
-        value: "54.0"
+        rateCents: "54.0"
     },
     PE: {
         label: "Prince Edward Island",
-        value: "48.5"
+        rateCents: "48.5"
     },
     QC: {
         label: "Quebec",
-        value: "51.0"
+        rateCents: "51.0"
     },
     SK: {
         label: "Saskatchewan",
-        value: "47.5"
+        rateCents: "47.5"
     },
     YT: {
         label: "Yukon",
-        value: "58.0"
+        rateCents: "58.0"
     },
 };
