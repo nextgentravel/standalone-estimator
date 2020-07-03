40feec9b10da2c8c2309a29f44cc771183b7522e
Mike Jenkins <kinetic@icloud.com>
2020-07-03 09:09:04 -0400

    Added validation to input-text component.

--- src/components/kilometrics.js
+++ src/components/kilometrics.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react"
 import locations from "../data/locations.js"
 import TextInput from "./input-text.js";
-
 import InputDatalist from "./input-datalist.js"
+import * as yup from "yup"
+
 
 const Kilometrics = () => {
 
@@ -12,11 +13,11 @@ const Kilometrics = () => {
     const [calculatedTotal, setCalculatedTotal] = useState(false);
     const [rate, setRate] = useState('');
 
+    const [loading, setLoading] = useState(false);
+    const [generalError, setGeneralError] = useState(false);
+
     const [validationWarnings, setValidationWarnings] = useState([]);
 
-    // We need to process the incoming province data to a format that
-    // the InputDatalist component expects
-    // this function runs once on render
     useEffect(() => {
         let provinceOptions = Object.keys(locations).map(location => {
             return {
@@ -25,31 +26,67 @@ const Kilometrics = () => {
             }
         })
         setProvinces(provinceOptions);
+
     }, []);
 
-    
+    const handleValidation = () => {
+        let provinceKeys = Object.keys(locations);
+        console.log(provinceKeys)
+        let target = {province, distance};
+        let schema = yup.object().shape({
+            province: yup
+                .string()
+                .required('Province is a required field')
+                .test(
+                    'Province is valid',
+                    'Province is not valid.',
+                    (value) => {
+                        return provinceKeys.includes(value)
+                    },
+                  ),
+            distance: yup
+                .number()
+                .integer()
+                .typeError('Kilometres Travelled must be a number')
+                .required('Distance is a required field'),
+        });
+        return schema.validate(target, {abortEarly: false})
+    }
 
     const handleSubmit = (e) => {
-        // unless we have the next line, the page is going to refresh
-        // since this is the standard behaviour of a 'submit' button
+        setLoading(true);
         e.preventDefault();
-        // this is where validation goes
-        let provinceRate = locations[province].rateCents
-        setRate(provinceRate)
-        let rateCalc = parseInt(provinceRate) * parseInt(distance) / 100;
-        // do stuff to make the hidden display part show here
-        setCalculatedTotal(rateCalc.toFixed(2));
+        handleValidation()
+            .then((valid) => {
+                setValidationWarnings([]);
+                let provinceRate = locations[province].rateCents
+                setRate(provinceRate)
+                let rateCalc = parseInt(provinceRate) * parseInt(distance) / 100;
+                setCalculatedTotal(rateCalc.toFixed(2));
+            })
+            .catch(err => {
+                setLoading(false);
+                setValidationWarnings(err.inner)
+            });
     }
+
+    const clearForm = () => {
+        document.getElementById("kilometrics-form").reset();
+        setProvinceValue('');
+        setDistance('');
+    }
+
 
     return (
         <>
             <div className="mb-4">
                 <h1>Find the correct rate for your kilometrics</h1>
                 <p className="lead">Taking your personal vehicle on a government trip? Refer to these rates.</p>
-                <form onSubmit={handleSubmit}>
+                <form id="kilometrics-form" onSubmit={handleSubmit}>
                     <InputDatalist validationWarnings={validationWarnings} setValidationWarnings={setValidationWarnings} label="Province/Territory of Travel:" name="province" options={provinces} updateValue={setProvinceValue} />
-                    <TextInput label="Kilometres Travelled" name="distance" updateValue={setDistance} />
+                    <TextInput validationWarnings={validationWarnings} setValidationWarnings={setValidationWarnings} label="Kilometres Travelled" name="distance" updateValue={setDistance} />
                     <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-secondary ml-2" onClick={clearForm}>Clear</button>
                 </form>
             </div>
             {rate !== '' && calculatedTotal !== '' && distance !== '' &&
@@ -59,16 +96,6 @@ const Kilometrics = () => {
                 </>
             }
         </>
-
-    // ## Note that this wasn't going to work
-    // ## as a react child can only ever be a single element.
-    // ## so wrap the whole element you're exporting in a <div> or <>
-    // ## like above
-    //    {results && distance !== 0 &&
-    //        <>
-    //        <p>The kilometics covered portion of this travel is {rateCalc}</p>
-    //        </>
-    //    }
     )
 }
 
