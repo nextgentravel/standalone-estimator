295fdaa56f4ab789196d0661bfdab99c6aa941b1
Mike Jenkins <kinetic@icloud.com>
2020-07-09 11:48:19 -0400

    Added language supports

--- src/components/header.js
+++ src/components/header.js
@@ -3,60 +3,60 @@ import React from "react"
 import Image from "../components/image"
 import Link from 'gatsby-link';
 import SelectLanguage from './languageSelect';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
-const Header = ({siteTitle, langs, showLanguageSelect, homeLink}) => (
-
-<header className="mb-4">
-  <div
-    style={{
-      backgroundColor: "#FAF2CC",
-      textAlign: "left",
-      verticalAlign: "middle"
-    }}
-  >
-    <div className="container py-3">
-      <button
+const Header = ({siteTitle, langs, showLanguageSelect, homeLink}) => {
+  const intl = useIntl();
+  return (
+    <header className="mb-4">
+      <div
         style={{
-          background: "#FEC04F",
-          border: "1px solid #000000",
-          borderRadius: 20,
-          marginRight: 10
+          backgroundColor: "#FAF2CC",
+          textAlign: "left",
+          verticalAlign: "middle"
         }}
       >
-        <strong><FormattedMessage id="alpha" /></strong>
-      </button>
-      <span><FormattedMessage id="underDevelopment" /></span>
-    </div>
-  </div>
-
-	<div className="container">
-		<div className="row mt-4">
-      <nav className="sr-only sr-only-focusable aurora-skip">
-        <a className="sr-only sr-only-focusable aurora-skip" href="#main-content">Skip to the main content</a>
-      </nav>
-      <div className="brand col-xs-9 col-sm-5 col-md-4" property="publisher" typeof="GovernmentOrganization">
-        <Link to={homeLink}>
-            <Image
-              filename="sig-blk-en.svg"
-              alt="Government of Canada"
-            />
-        </Link>
+        <div className="container py-3">
+          <button
+            style={{
+              background: "#FEC04F",
+              border: "1px solid #000000",
+              borderRadius: 20,
+              marginRight: 10
+            }}
+          >
+            <strong><FormattedMessage id="alpha" /></strong>
+          </button>
+          <span><FormattedMessage id="underDevelopment" /></span>
+        </div>
       </div>
-      <section className='col-xs-3 ml-auto mr-3'>
-        {showLanguageSelect &&
-          <>
-            <h2 className="d-none sr-only sr-only-focusable aurora-skip">Language selection</h2>
-            <SelectLanguage langs={langs} />
-          </>
-        }
-      </section>
-		</div>
-	</div>
-</header>
 
-
-)
+      <div className="container">
+        <div className="row mt-4">
+          <nav className="sr-only sr-only-focusable aurora-skip">
+            <a className="sr-only sr-only-focusable aurora-skip" href="#main-content"><FormattedMessage id="skipToMain" /></a>
+          </nav>
+          <div className="brand col-xs-9 col-sm-5 col-md-4" property="publisher" typeof="GovernmentOrganization">
+            <Link to={homeLink}>
+                <Image
+                  filename="sig-blk-en.svg"
+                  alt={ intl.formatMessage({ id: 'governmentOfCanada' })}
+                />
+            </Link>
+          </div>
+          <section className='col-xs-3 ml-auto mr-3'>
+            {showLanguageSelect &&
+              <>
+                <h2 className="sr-only sr-only-focusable aurora-skip"><FormattedMessage id="languageSelection" /></h2>
+                <SelectLanguage langs={langs} />
+              </>
+            }
+          </section>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
