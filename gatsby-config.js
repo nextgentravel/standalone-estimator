0480c84e7d1d0fd9c5a7d476ded0d790dbea82c6
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-07-30 11:27:45 -0300

    setting up google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,6 +109,19 @@ module.exports = {
       }
     },
     "gatsby-plugin-use-query-params",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-173947223-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
