5fe4eb01dd71a37fcec7a00f17f4ca59e1b36215
Mike Jenkins <kinetic@icloud.com>
2020-08-07 10:53:48 -0400

    Removed inline css

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -5,7 +5,7 @@ import { FaTicketAlt } from 'react-icons/fa';
 import { FaPlaneDeparture } from 'react-icons/fa';
 import { FaFileInvoiceDollar } from 'react-icons/fa';
 import { FaChevronRight } from 'react-icons/fa';
-import { FaCalculator } from 'react-icons/fa';
+// import { FaCalculator } from 'react-icons/fa';
 import { FaMapMarker } from 'react-icons/fa';
 import { FaCar } from 'react-icons/fa';
 import SEO from "../components/seo"
@@ -16,23 +16,16 @@ const IndexPage = () => (
   <SEO title="Home" />
   <w-screen mt-4="true" fluid="true" id="container">
     <div>
-      <main style={{borderColor: 'red', borderWidth: 5}} id="main-content" role="main">
+      <main id="main-content" role="main">
         <div className="container mt-4">
           <h3 className="text-center mb-4">Get Started</h3>
-          <p className="text-center mb-5">Select the part of the journey you need support with or launch our travel assistant for a guided travel experience.</p>
+          <p className="text-center mb-5">Select the part of the journey you need support with or launch our travel assistant for a guided travel experience.  You can use this guide offline, even when you are not connected to the internet.</p>
           <div className="row mb-3 mt-3">
               <section className="col-sm-3 text-center mb-5">
                 <div className="h-100 d-flex flex-column">
-                  <div className="mt-2 mb-4" style={{
-                      backgroundColor: '#E7A948',
-                      paddingTop: 20,
-                      borderRadius: 100,
-                      width: 150,
-                      height: 150,
-                      margin: 'auto',
-                      textAlign: 'center',
-                      verticalAlign: 'middle',
-                  }}><FaClipboardList size="100" color="#fff" /></div>
+                  <div className="icon-background icon-background-plan mt-2 mb-4">
+                    <FaClipboardList size="100" color="#fff" />
+                  </div>
                   <h4 className="mb-4">Plan</h4>
                   <p className="mb-4">Plan ahead and get ready for your business trip</p>
                   <div className="mt-auto w-100">
@@ -40,27 +33,13 @@ const IndexPage = () => (
                   </div>
                 </div>
                 <FaChevronRight
+                  className="home-chevron d-none d-sm-block"
                   size="30"
-                  style={{
-                    position: 'absolute',
-                    right: -15,
-                    top: 65,
-                    color: '#9E9E9E',
-                  }}
                 />
               </section>
               <section className="col-sm-3 text-center mb-5">
                 <div className="h-100 d-flex flex-column">
-                  <div className="mt-2 mb-4" style={{
-                      backgroundColor: '#CC4859',
-                      paddingTop: 20,
-                      borderRadius: 100,
-                      width: 150,
-                      height: 150,
-                      margin: 'auto',
-                      textAlign: 'center',
-                      verticalAlign: 'middle',
-                  }}><FaTicketAlt size="100" color="#fff" /></div>
+                  <div className="icon-background icon-background-book mt-2 mb-4"><FaTicketAlt size="100" color="#fff" /></div>
                   <h4 className="mb-4">Book</h4>
                   <p className="mb-4">
                     Secure your tickets and make travel arrangements
@@ -70,27 +49,15 @@ const IndexPage = () => (
                   </div>
                 </div>
                 <FaChevronRight
+                  className="home-chevron d-none d-sm-block"
                   size="30"
-                  style={{
-                    position: 'absolute',
-                    right: -15,
-                    top: 65,
-                    color: '#9E9E9E',
-                  }}
                 />
               </section>
               <section className="col-sm-3 text-center mb-5">
                 <div className="h-100 d-flex flex-column">
-                  <div className="mt-2 mb-4" style={{
-                      backgroundColor: '#047789',
-                      paddingTop: 20,
-                      borderRadius: 100,
-                      width: 150,
-                      height: 150,
-                      margin: 'auto',
-                      textAlign: 'center',
-                      verticalAlign: 'middle',
-                  }}><FaPlaneDeparture size="100" color="#fff" /></div>
+                  <div className="icon-background icon-background-travel mt-2 mb-4">
+                    <FaPlaneDeparture size="100" color="#fff" />
+                  </div>
                   <h4 className="mb-4">Travel</h4>
                   <p className="mb-4">
                     Everything you need when you're travelling
@@ -100,27 +67,15 @@ const IndexPage = () => (
                   </div>
                 </div>
                 <FaChevronRight
+                  className="home-chevron d-none d-sm-block"
                   size="30"
-                  style={{
-                    position: 'absolute',
-                    right: -15,
-                    top: 65,
-                    color: '#9E9E9E',
-                  }}
                 />
               </section>
               <section className="col-sm-3 text-center mb-5">
                 <div className="h-100 d-flex flex-column">
-                  <div className="mt-2 mb-4" style={{
-                      backgroundColor: '#043B59',
-                      paddingTop: 20,
-                      borderRadius: 100,
-                      width: 150,
-                      height: 150,
-                      margin: 'auto',
-                      textAlign: 'center',
-                      verticalAlign: 'middle',
-                  }}><FaFileInvoiceDollar size="100" color="#fff" /></div>
+                  <div className="icon-background icon-background-expense mt-2 mb-4">
+                    <FaFileInvoiceDollar size="100" color="#fff" />
+                  </div>
                   <h4 className="mb-4">Expense</h4>
                   <p className="mb-4">Filing trip expenses and get reimbursed</p>
                   <div className="mt-auto w-100">
@@ -130,22 +85,26 @@ const IndexPage = () => (
               </section>
           </div>
         </div>
-        <div style={{
-          backgroundColor: '#F8F8F8',
-        }}>
+        <div class="bg-light">
           <div className="container">
             <div className="d-flex justify-content-center">
-              <div className="text-center m-5">
-                <FaCalculator size="40" />
-                <p className="mt-2">Trip Estimator</p>
+              {/* <div className="text-center m-5">
+                <Link to="/en/after">
+                  <FaCalculator size="40" />
+                  <p className="mt-2">Trip Estimator</p>
+                </Link>
+              </div> */}
+              <div className="tool-link-item text-center m-5">
+                <Link to="/en/rates">
+                  <FaMapMarker size="40" />
+                  <p className="mt-2">City Rates and Limits</p>
+                </Link>
               </div>
-              <div className="text-center m-5">
-                <FaMapMarker size="40" />
-                <p className="mt-2">City Rates and Limits</p>
-              </div>
-              <div className="text-center m-5">
-                <FaCar size="40" />
-                <p className="mt-2">Kilometric Rates</p>
+              <div className="tool-link-item text-center m-5">
+                <Link to="/en/kilometrics">
+                  <FaCar size="40" />
+                  <p className="mt-2">Kilometric Rates</p>
+                </Link>
               </div>
             </div>
           </div>
