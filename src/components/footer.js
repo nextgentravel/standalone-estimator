d498e6e5885552b3e5eef3ea8404860cd77d6dad
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-08-07 17:03:24 -0300

    not quite right footer - images are issue

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,87 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Image from "../components/image"
 import { FormattedMessage } from 'react-intl';
 
 const Footer = ({ siteTitle, homeLink }) => (
   <footer className="footer">
-    <div className="container">
+    <div className="container bg-dark col-12 footer-deco py-5">
       <ul className="pl-0">
         <li>
-          <Link to={`${homeLink}`}><FormattedMessage id="home" /></Link>
+          <a href="" className="text-light">
+            <FormattedMessage id="knowledge" />
+          </a>
         </li>
         <li>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSf9y3VY3ADLpQ4kQLGvOo4cIdEEi5Hs3en-0lWRc4wQeTRheg/viewform?usp=sf_link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FormattedMessage id="suggestAnUpdate" />
+          <a href="" className="text-light">
+            <FormattedMessage id="training" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="submitquestion" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+          <FormattedMessage id="contact" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="nextgen" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="travelcard" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="travelcontacts" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="advisories" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="text-light">
+            <FormattedMessage id="estimator" />
           </a>
         </li>
       </ul>
-      
-      <ul className="pl-0">
+    </div>
+    <div className="container col-12">
+      <ul className="col-9">
         <li>
-          <a href="https://github.com/nextgentravel/travel-guidebook-client">
-            <small><FormattedMessage id="viewThisPageOnGithub" /></small>
+          <a href="">
+            <FormattedMessage id="socialmedia" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FormattedMessage id="mobileapp" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FormattedMessage id="terms" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FormattedMessage id="privacy" />
           </a>
         </li>
       </ul>
+      <div className="col-2 float-right">
+        <Image
+          filename="footer-tag.svg"
+        />
+      </div>
     </div>
   </footer>
 )
