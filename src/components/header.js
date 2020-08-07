dfee08db72405b9f48c5acc36b1b7aa55e2e1fc7
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-08-07 12:26:36 -0300

    small colour and space fixes

--- src/components/header.js
+++ src/components/header.js
@@ -59,7 +59,7 @@ const Header = ({siteTitle, langs, showLanguageSelect, homeLink}) => {
         <h1 className="col-10 col-md-6 mx-auto text-center text-light">GC Travel Guide</h1>
       </div>
       <div className="bg-light col-12">
-        <section className='col-12 col-md-6 mx-auto pt-4 pb-4 '>
+        <section className='col-12 col-md-6 mx-auto pt-4 pb-4'>
           <SearchForm />
         </section>
       </div>
