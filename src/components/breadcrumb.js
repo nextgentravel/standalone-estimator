9df29a33f574a029d7a9ea1a87d5d511607fa624
Mike Jenkins <kinetic@icloud.com>
2020-08-10 12:28:10 -0400

    Breadcrumb tidy up

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -8,7 +8,7 @@ const Breadcrumbs = ({ pageTitle, homeLink }) => (
     <nav aria-label="breadcrumb">
         <h2 className="sr-only sr-only-focusable aurora-skip"><FormattedMessage id="breadCrumbAria" /></h2>
         <ol className="breadcrumb pl-0">
-            <li className="breadcrumb-item"><Link to={homeLink}><FormattedMessage id="siteTitle" /></Link></li>
+            <li className="breadcrumb-item"><Link to={homeLink} className="text-dark"><FormattedMessage id="siteTitle" /></Link></li>
             <li className="breadcrumb-item active" aria-current="page">{pageTitle}</li>
         </ol>
     </nav>
