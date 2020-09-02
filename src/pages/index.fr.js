dcd088ecd4e34cf1f067cc82e0d962f226c2739b
Mike Jenkins <kinetic@icloud.com>
2020-09-02 15:26:04 -0400

    Master home layout.

--- src/pages/index.fr.js
+++ src/pages/index.fr.js
@@ -1,33 +1,8 @@
 import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo";
-import ContentPanel from "../components/content-panel";
-import ToolPanel from "../components/tool-panel";
-import { FormattedMessage } from "react-intl";
+import HomeLayout from '../components/home-layout'
 
 const IndexPage = () => (
-  <Layout>
-  <SEO title="Home" />
-  <w-screen mt-4="true" fluid="true" id="container">
-    <div>
-      <main id="main-content" role="main">
-        <div className="container mt-4">
-          <h2 className="text-center mb-4 font-weight-bold"><FormattedMessage id="indexTitle" /></h2>
-          <p className="text-center mb-5"><FormattedMessage id="indexLead"/></p>
-          <div className="row mb-3 mt-3">
-            <ContentPanel />
-          </div>
-        </div>
-        <div className="bg-light">
-          <div className="container">
-            <ToolPanel />
-          </div>
-        </div>
-      </main>
-    </div>
-  </w-screen>
-</Layout>
+  <HomeLayout />
 )
 
 export default IndexPage
-
