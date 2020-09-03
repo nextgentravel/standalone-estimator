d12843d257ccf8d41c77436d185bbede89f15933
Mike Jenkins <kinetic@icloud.com>
2020-09-03 16:16:23 -0400

    Added gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -196,8 +196,12 @@ module.exports = {
         respectDNT: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-hotjar`,
+      options: {
+        id: process.env.HOTJAR_ID,
+        sv: process.env.HOTJAR_SNIPPET_VERSION
+      },
+    },
   ],
 }
