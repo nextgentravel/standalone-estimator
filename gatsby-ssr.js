248958079895461b0c7ad1a05a4bad002acc6c9d
Mike Jenkins <kinetic@icloud.com>
2020-09-03 10:16:45 -0400

    Fix for gatsby SSR?

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,9 +6,24 @@
 
 // You can delete this file if you're not using it
 
-const React = require("react")
-var reactIntl = require("react-intl");
+import "./src/styles/style.scss"
+import React from 'react'
+import { getCurrentLangKey } from 'ptz-i18n';
+import { IntlProvider } from 'react-intl';
 
-exports.wrapPageElement = ({ element }) => {
-  return <reactIntl.IntlProvider locale="en">{element}</reactIntl.IntlProvider>
+import i18nMessages from './src/data/messages';
+
+import { globalHistory } from "@reach/router"
+
+import languages from './src/data/languages'
+
+export const wrapPageElement = ({ element }) => {
+    const url = globalHistory.location.pathname;
+    const { langs, defaultLangKey } = languages;
+    const langKey = getCurrentLangKey(langs, defaultLangKey, url);
+    return (
+        <IntlProvider locale={langKey} messages={i18nMessages[langKey]}>
+            {element}
+        </IntlProvider>
+    )
 }
