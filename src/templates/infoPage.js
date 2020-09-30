902f291d5d6e5d001e421f0e8b7bc97128dfeae5
Mike Jenkins <kinetic@icloud.com>
2020-09-30 13:32:36 -0400

    Updated template with jumpTo link

--- src/templates/infoPage.js
+++ src/templates/infoPage.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import Breadcrumbs from "../components/breadcrumb"
 import SEO from "../components/seo"
 import { FormattedMessage } from 'react-intl';
-
+import { navigate } from "gatsby"
 import { getCurrentLangKey } from 'ptz-i18n';
 import 'intl';
 import { globalHistory } from "@reach/router"
@@ -15,15 +15,40 @@ export default ({ data }) => {
   const { langs, defaultLangKey } = data.site.siteMetadata.languages;
   const langKey = getCurrentLangKey(langs, defaultLangKey, url);
   const homeLink = `/${langKey}/`;
+
+  const jumpTo = (e) => {
+    navigate(
+      e.target.value,
+      { replace: true }
+    )
+  }
+
   return (
     <Layout>
       <main id="main-content">
-        <SEO title={post.frontmatter.title} />
         <Breadcrumbs pageTitle={post.frontmatter.title} homeLink={homeLink} />
+        <SEO title={post.frontmatter.title} />
         <div className="hero-holder">
           <div className="container">
             <nav className="skiphold" aria-label="sidebar skiplink"><a className="sr-only sr-only-focusable aurora-skip skiplink" id="sidebar-skiplink" href="#sidebar"><FormattedMessage id="skipToSide"/></a></nav>
-            <h2 className="display-5">{post.frontmatter.title}</h2>
+            <div className="row mb-4">
+              <div className="col-sm-8"><h2 className="display-5">{post.frontmatter.title}</h2></div>
+              <div className="col-sm-2 ml-auto">
+              <div class="form-group">
+                <select onChange={jumpTo} class="custom-select text-secondary align-middle">
+                  <option value="">Jump to...</option>
+                  {post.frontmatter.jumpTo.map((item) => {
+                    return (
+                      <option value={`${homeLink}${item.link}`}>{item.label}</option>
+                    )
+                  })}
+                </select>
+                <div class="invalid-feedback">Example invalid custom select feedback</div>
+              </div>
+
+              </div>
+            </div>
+            
             <p className="lead">
               {post.frontmatter.lead}
             </p>
@@ -55,6 +80,10 @@ export const query = graphql`
       frontmatter {
         title
         lead
+        jumpTo {
+          label
+          link
+        }
       }
     }
   }
