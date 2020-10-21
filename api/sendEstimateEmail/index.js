2d8806bc06e5a57506354ee01e477149f7126579
Mike Jenkins <kinetic@icloud.com>
2020-10-21 16:04:59 -0400

    Mail test.

---
+++ api/sendEstimateEmail/index.js
@@ -0,0 +1,28 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+      to: req.body.to, // Change to your recipient
+      from: 'gctravelapp@gmail.com', // Change to your verified sender
+      subject: 'Travel Estimate',
+      text: 'Here it is!',
+      html: '<strong>Here it is!</strong>',
+    }
+
+    sgMail
+        .send(msg)
+        .then(() => {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: 'email sent'
+            };
+        })
+        .catch((error) => {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: error
+            };
+        })
+}
