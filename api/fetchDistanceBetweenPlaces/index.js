1cfd151d8a9566b0a348d2be3c406d92066e1065
Mike Jenkins <kinetic@icloud.com>
2020-10-24 17:13:49 -0400

    Kilometrics/distance feature is working

--- api/fetchDistanceBetweenPlaces/index.js
+++ api/fetchDistanceBetweenPlaces/index.js
@@ -1,13 +1,22 @@
+const fetch = require('node-fetch');
+global.Headers = fetch.Headers;
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const googleToken = process.env.GOOGLE_API_TOKEN;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    let origin = req.query.origin;
+    let destination = req.query.destination;
+
+    await fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin}&destinations=${destination}&key=${googleToken}`)
+        .then(response => response.text())
+        .then(result => {
+            context.log('result', result)
+            context.res = {
+                body: result
+            };
+        })
+        .catch(error => context.log('error', error));
+
 }
