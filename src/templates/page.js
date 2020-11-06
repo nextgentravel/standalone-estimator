e6769980275a0365c808a85ff8dcc52fd3059946
Mike Jenkins <kinetic@icloud.com>
2020-11-06 09:16:08 -0500

    Preview test.

---
+++ src/templates/page.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { withPreview } from 'gatsby-source-prismic'
+
+import { Layout } from '../components/layout'
+
+// `data` will automatically include preview data when previewing from Prismic.
+const PageTemplate = ({ data }) => (
+  <Layout>
+    <h1>{data.prismicTravelSection.data.title.text}</h1>
+  </Layout>
+)
+
+export default withPreview(PageTemplate)
+
+export const query = graphql`
+  query PageTemplate($uid: String!) {
+    prismicTravelSection(uid: { eq: $uid }) {
+      data {
+        title {
+          text
+        }
+      }
+    }
+  }
+`
