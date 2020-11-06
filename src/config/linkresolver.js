a78f83ac4fac87df5d463e86fc9afa227ec3736e
Mike Jenkins <kinetic@icloud.com>
2020-11-06 10:34:03 -0500

    consoling

--- src/config/linkresolver.js
+++ src/config/linkresolver.js
@@ -1,5 +1,7 @@
 const linkResolver = ({ node, key, value }) => doc => {
+    console.log('doc.type', doc.type);
     if (doc.type === "travel_step") {
+      console.log('doc.data.belongs_to.uid', doc.data.belongs_to.uid)
       return `/en/${doc.data.belongs_to.uid}`
     }
     if (doc.type === "travel_section") {
