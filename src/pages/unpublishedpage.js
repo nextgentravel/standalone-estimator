b9acaec55b3233eeaeb78a5931924ca507092a95
Mike Jenkins <kinetic@icloud.com>
2020-11-10 15:14:49 -0500

    Split into components, individual preview

--- src/pages/unpublishedpage.js
+++ src/pages/unpublishedpage.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
 
 import PageTemplate from '../templates/page'
+import TravelStep from '../components/travel-step'
+import FaqQuestion from '../components/faq-question'
 
 export const UnpublishedPage = (props) => {
   const previewData = window.__PRISMIC_PREVIEW_DATA__
   // => Perform any logic from previewData to determine the correct page or template component to use.
-  console.log('previewData: ', previewData);
-  console.log('props: ', props);
+
+  let previewType = Object.keys(previewData)[0];
+
+  switch (previewType) {
+    case 'prismicTravelStep':
+      return <div className="container"><TravelStep index={0} data={previewData[previewType].data} /></div>
+      break;
+      case 'prismicFaqQuestion':
+        return <div className="container"><FaqQuestion index={0} data={previewData[previewType].data} /></div>
+        break;
+    default:
+      return <div className="container"><p>Unknown Preview Type</p></div>
+      break;
+  }
 
   return <PageTemplate {...props} data={previewData} />
 }
