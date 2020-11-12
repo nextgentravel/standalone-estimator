2669c8831dfebc7f98bc4c7a6641325c929de201
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-11-12 16:03:26 -0400

    current progress on fixing invalid character from ie

--- src/api-calls/amadeusAirportCode.js
+++ src/api-calls/amadeusAirportCode.js
@@ -1,5 +1,5 @@
 module.exports = async function (lat, long, access_token) {
-    return fetch(`/api/amadeusAirportCode?latitude=${lat}&longitude=${long}&access_token=${access_token}`, {
+    return fetch("/api/amadeusAirportCode?latitude=" + lat + "&longitude=" + long + "&access_token=" + access_token, {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
