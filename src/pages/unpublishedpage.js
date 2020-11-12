cd718668101ab3f4cef72f80c555d5e91315d895
Mike Jenkins <kinetic@icloud.com>
2020-11-12 13:45:24 -0500

    All previews working.

--- src/pages/unpublishedpage.js
+++ src/pages/unpublishedpage.js
@@ -3,6 +3,9 @@ import * as React from 'react'
 import PageTemplate from '../templates/page'
 import TravelStep from '../components/travel-step'
 import FaqQuestion from '../components/faq-question'
+import DoormatPanelItem from '../components/doormat-panel-item'
+import Breadcrumbs from "../components/breadcrumb"
+import SEO from "../components/seo"
 
 export const UnpublishedPage = (props) => {
   let previewData = {}
@@ -19,6 +22,8 @@ export const UnpublishedPage = (props) => {
 
   let previewType = Object.keys(previewData)[0];
 
+  console.log(previewType)
+
   switch (previewType) {
     case 'prismicTravelStep':
       return <div className="container"><TravelStep index={0} data={previewData[previewType].data} /></div>
@@ -34,6 +39,54 @@ export const UnpublishedPage = (props) => {
         </div>
       )
       break;
+    case 'prismicDoormat':
+      console.log('previewData[previewType]', previewData[previewType])
+      return (
+        <div className="container mt-5">
+          <DoormatPanelItem
+            image={previewData[previewType].data.image.url}
+            alt=""
+            linkTo={previewData[previewType].data.link}
+            linkNewWindow={previewData[previewType].data.link_new_window}
+            title={previewData[previewType].data.title.text}
+            content={previewData[previewType].data.lead.text}
+          />
+        </div>
+      )
+      break;
+      case 'prismicGenericContentPage':
+        return (
+          <main id="main-content">
+            <Breadcrumbs pageTitle={previewData[previewType].data.title.text} homeLink={''} />
+            <SEO title={previewData[previewType].data.title.text} />
+            <div className="hero-holder">
+              <div className="container">
+                <nav className="skiphold" aria-label="sidebar skiplink"><a className="sr-only sr-only-focusable aurora-skip skiplink" id="sidebar-skiplink" href="#sidebar"></a></nav>
+                <div className="row mb-4">
+                  <div className="col-sm-8"><h2 className="display-5">{previewData[previewType].data.title.text}</h2></div>
+                </div>
+                <div className="lead" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.lead.html }}>
+                </div>
+              </div>
+            </div>
+            <div className="container p-0">
+              <article className="content-left col-xs-12 col-sm-12 col-md-12" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.content.html }}>
+              </article>
+            </div>
+          </main>
+        )
+        break;
+        case 'prismicHomepage':
+          return (
+            <div id="container">
+              <div className="container mt-4">
+                <h2 className="mb-4 font-weight-bold">{previewData[previewType].data.title.text}</h2>
+                <div dangerouslySetInnerHTML={{__html: previewData[previewType].data.lead.html}}></div>
+              </div>
+            </div>
+          )
+          break;
+
     default:
       return <div className="container"><p>Unknown Preview Type</p></div>
       break;
