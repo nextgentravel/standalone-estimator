4013ecc6ddb4ea8b2457f01456f798f8d6962971
Mike Jenkins <kinetic@icloud.com>
2020-11-24 13:22:14 -0500

    Removed breaks, unreachable code.

--- src/pages/unpublishedpage.js
+++ src/pages/unpublishedpage.js
@@ -27,10 +27,9 @@ export const UnpublishedPage = (props) => {
   switch (previewType) {
     case 'prismicTravelStep':
       return <div className="container"><TravelStep index={0} data={previewData[previewType].data} /></div>
-      break;
     case 'prismicFaqQuestion':
-        return <div className="container"><FaqQuestion index={0} data={previewData[previewType].data} /></div>
-        break;
+      return <div className="container"><FaqQuestion index={0} data={previewData[previewType].data} /></div>
+
     case 'prismicTravelSection':
       return (
         <div className="container">
@@ -38,9 +37,7 @@ export const UnpublishedPage = (props) => {
           <div className="lead" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.lead.html }} />
         </div>
       )
-      break;
     case 'prismicDoormat':
-      console.log('previewData[previewType]', previewData[previewType])
       return (
         <div className="container mt-5">
           <DoormatPanelItem
@@ -53,43 +50,40 @@ export const UnpublishedPage = (props) => {
           />
         </div>
       )
-      break;
-      case 'prismicGenericContentPage':
+    case 'prismicGenericContentPage':
+      return (
+        <main id="main-content">
+          <Breadcrumbs pageTitle={previewData[previewType].data.title.text} homeLink={''} />
+          <SEO title={previewData[previewType].data.title.text} />
+          <div className="hero-holder">
+            <div className="container">
+              <nav className="skiphold" aria-label="sidebar skiplink"><a className="sr-only sr-only-focusable aurora-skip skiplink" id="sidebar-skiplink" href="#sidebar"></a></nav>
+              <div className="row mb-4">
+                <div className="col-sm-8"><h2 className="display-5">{previewData[previewType].data.title.text}</h2></div>
+              </div>
+              <div className="lead" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.lead.html }}>
+              </div>
+            </div>
+          </div>
+          <div className="container p-0">
+            <article className="content-left col-xs-12 col-sm-12 col-md-12" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.content.html }}>
+            </article>
+          </div>
+        </main>
+      )
+
+      case 'prismicHomepage':
         return (
-          <main id="main-content">
-            <Breadcrumbs pageTitle={previewData[previewType].data.title.text} homeLink={''} />
-            <SEO title={previewData[previewType].data.title.text} />
-            <div className="hero-holder">
-              <div className="container">
-                <nav className="skiphold" aria-label="sidebar skiplink"><a className="sr-only sr-only-focusable aurora-skip skiplink" id="sidebar-skiplink" href="#sidebar"></a></nav>
-                <div className="row mb-4">
-                  <div className="col-sm-8"><h2 className="display-5">{previewData[previewType].data.title.text}</h2></div>
-                </div>
-                <div className="lead" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.lead.html }}>
-                </div>
-              </div>
+          <div id="container">
+            <div className="container mt-4">
+              <h2 className="mb-4 font-weight-bold">{previewData[previewType].data.title.text}</h2>
+              <div dangerouslySetInnerHTML={{__html: previewData[previewType].data.lead.html}}></div>
             </div>
-            <div className="container p-0">
-              <article className="content-left col-xs-12 col-sm-12 col-md-12" dangerouslySetInnerHTML={{ __html: previewData[previewType].data.content.html }}>
-              </article>
-            </div>
-          </main>
+          </div>
         )
-        break;
-        case 'prismicHomepage':
-          return (
-            <div id="container">
-              <div className="container mt-4">
-                <h2 className="mb-4 font-weight-bold">{previewData[previewType].data.title.text}</h2>
-                <div dangerouslySetInnerHTML={{__html: previewData[previewType].data.lead.html}}></div>
-              </div>
-            </div>
-          )
-          break;
 
     default:
       return <div className="container"><p>Unknown Preview Type</p></div>
-      break;
   }
 
   return <PageTemplate {...props} data={previewData} />
