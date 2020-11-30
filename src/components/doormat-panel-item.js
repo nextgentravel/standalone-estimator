f7592ceb5b4ae965cf9655223e4025044b02c127
Mike Jenkins <kinetic@icloud.com>
2020-11-30 14:10:22 -0500

    Fixed merge conflicts

--- src/components/doormat-panel-item.js
+++ src/components/doormat-panel-item.js
@@ -2,25 +2,6 @@ import React from "react";
 import { Link } from "gatsby";
 import { Card } from "react-bootstrap"
 
-<<<<<<< HEAD
-const DoormatPanelItem = ({ title, content, linkTo, image, alt, linkNewWindow }) => (
-    <div className="col-sm-6 mb-4">
-        <Card className="bg-light h-100">
-            <img
-                className="doormat-image m-2"
-                src={image}
-                alt={alt}
-            />
-            <Card.Body>
-                <Card.Title><Link to={linkTo} target={linkNewWindow ? '_blank' : ''}>{title}</Link></Card.Title>
-                <Card.Text>
-                    {content}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    </div>
-)
-=======
 const DoormatPanelItem = ({ title, content, linkTo, image, alt, linkNewWindow }) => {
     return (
         <div className="col-sm-6 mb-4">
@@ -47,6 +28,5 @@ const DoormatPanelItem = ({ title, content, linkTo, image, alt, linkNewWindow })
         </div>
     )
 }
->>>>>>> bugs/dev-cleanup
 
 export default DoormatPanelItem
