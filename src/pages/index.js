17c9cf9f4261666663108f2fa72ee7618904702f
Mike Jenkins <kinetic@icloud.com>
2020-12-04 12:44:04 -0500

    Removed homepage and homepage CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,9 @@
 import React from "react"
-import SEO from "../components/seo"
-import { Link } from "gatsby"
-import { FaCircle } from 'react-icons/fa';
 
 const IndexPage = () => (
   <main id="main-content" role="main">
-    <div id="bg"></div>
-    <SEO title="Travel Guide - Language Select" />
-    <div className="card lang">
-      <div className="card-body d-flex justify-content-center">
-        {/* <image></image> */}
-        <h1 className="sr-only">Travel guidebook language selection</h1>
-        <Link to="/fr" className="btn btn-primary m-2 p-2 pl-3 pr-3">Français</Link>
-        <Link to="/en" className="btn btn-primary m-2 p-2 pl-3 pr-3">English</Link>
-      </div>
-      <div className="card-body bg-light">
-        <div className="col-6 float-left">
-          <a className="text-dark" href="/">Avis</a>
-          &nbsp;<FaCircle size={5} className="text-secondary" />&nbsp;
-          <a className="text-dark" href="/">Terms and conditions</a>
-        </div>
-        <div className="col-6">
-          {/* <image></image> */}
-        </div>
-      </div>
-    </div>
+        <h1 className="sr-only">Travel guidebook</h1>
+        <p>Once deployed, this page is invisible.  The web server will re-direct to /en, where language detection is done.</p>
   </main>
 )
 export default IndexPage
