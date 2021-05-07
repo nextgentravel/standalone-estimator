aa1b33c493d3b2b29834d961d4a9a5b7ea5547bb
Mike Jenkins <kinetic@icloud.com>
2021-05-07 14:30:06 -0400

    Updated meal rates.

--- src/data/meals.js
+++ src/data/meals.js
@@ -1,26 +1,26 @@
 module.exports = {
     YT: {
-        breakfast: 23.05,
-        lunch: 21.15,
-        dinner: 61.00,
+        breakfast: 22.90,
+        lunch: 21.00,
+        dinner: 60.60,
         incidentals: 17.30
     },
     TN: {
-        breakfast: 24.85,
-        lunch: 30.15,
-        dinner: 64.50,
+        breakfast: 24.80,
+        lunch: 30.05,
+        dinner: 64.35,
         incidentals: 17.30
     },
     NU: {
-        breakfast: 27.85,
-        lunch: 33.80,
-        dinner: 90.05,
+        breakfast: 27.75,
+        lunch: 33.65,
+        dinner: 89.70,
         incidentals: 17.30
     },
     CAN: {
-        breakfast: 20.80,
-        lunch: 21.05,
-        dinner: 51.65,
+        breakfast: 21.10,
+        lunch: 21.35,
+        dinner: 52.40,
         incidentals: 17.30
     },
 };
