f4051bb496b02632d030f9195469205e87b0a0d7
Mike Jenkins <kinetic@icloud.com>
2021-07-29 08:03:28 -0400

    Initial switch over to pspc header/footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -25,32 +25,93 @@ const Footer = (props) => {
   let messages = allPrismicStandaloneestimatorHomepage.nodes.find(function(o){ return o.lang === locale }).data;
 
   return (
-    <footer className="footer">
-      <div className="bg-dark py-5 footer-deco">
-        <div className="container">
-          <div className="row ml-1">
-                <div className="col-sm-8">
-                  <span className="footer-text text-white" dangerouslySetInnerHTML={{ __html: messages.footer_text.html }}></span>
-                </div>
+    <footer role="contentinfo" id="wb-info" class="wet-visible-sm wet-visible-md wet-visible-lg wb-navcurr">
+      <div class="wet-container">
+        <nav role="navigation">
+          <h2 class="wb-inv">Site Information</h2>
+          <div>
+            <p><a href="/gc/avis-terms-eng.html" rel="license">Terms and conditions</a></p>
+          </div>
+          <div class="clear"></div>
+          <div class="wet-row">
+            <section class="wet-col-sm-4 wet-col-lg-4">
+              <h3>About <abbr title="Public Services and Procurement Canada">PSPC</abbr></h3>
+              <ul class="wet-list-unstyled">
+                <li><a href="/gc/activ-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Our activities']);">Our
+                    activities</a></li>
+                <li><a href="/rfp-psr/index-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Public Service Renewal stories']);">Public
+                    Service Renewal stories</a></li>
+                <li><a href="/gc/region-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Our regional offices']);">Our
+                    regional offices</a></li>
+                <li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/pm-dp/index-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'PSPC departmental policies']);"><abbr
+                      title="Public Services and Procurement Canada">PSPC</abbr> departmental
+                    policies</a></li>
+                <li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/sc-cs/index-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Client service']);">Client
+                    service</a></li>
+                <li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/ba-ao/index-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'PSPC Accessibility Office']);"><abbr
+                      title="Public Services and Procurement Canada">PSPC</abbr> Accessibility
+                    Office</a></li>
+              </ul>
+            </section>
+            <section class="wet-col-sm-4 wet-col-lg-4">
+              <h3>Contact us</h3>
+              <ul class="wet-list-unstyled">
+                <li><a href="/gc/questions-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'General inquiries']);">General
+                    inquiries</a></li>
+                <li><a href="http://gcdirectory-gcannuaire.ssc-spc.gc.ca/en/GCD/?pgid=014&amp;dn=OU=PSPC-SPAC,O=GC,C=CA"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Find an employee [GEDS]']);">Find
+                    an employee [<abbr title="GCdirectory">GEDS</abbr>]</a></li>
+                <li><a href="/bi-rp/issues-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Report building and office issues (National Service Call Centre)']);">Report
+                    building and office issues<br />
+                    (National Service Call Centre)</a>
+                </li>
+              </ul>
+            </section>
+            <section class="wet-col-sm-4 wet-col-lg-4">
+              <h3>Related sites</h3>
+              <ul class="wet-list-unstyled">
+                <li><a href="http://www.tpsgc-pwgsc.gc.ca/comm/index-eng.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'PSPC Internet site']);"><abbr
+                      title="Public Services and Procurement Canada">PSPC</abbr> Internet site</a>
+                </li>
+                <li><a href="http://intranet.canada.ca/index-eng.asp"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'GCIntranet']);">GCIntranet</a>
+                </li>
+                <li><a href="https://www.canada.ca/en/public-service-commission/jobs/services/gc-jobs.html"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'Public service jobs']);">Public
+                    service jobs</a></li>
+                <li><a href="http://gcconnex.gc.ca/"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'GCconnex']);">GCconnex</a>
+                </li>
+                <li><a href="http://www.gcpedia.gc.ca/wiki/?setlang=en"
+                    onclick="javascript:_paq.push(['trackEvent', 'Pied de page-Footer-EN', 'GCpedia']);">GCpedia</a>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </nav>
+      </div>
+      <div class="wet-brand">
+        <div class="wet-container">
+          <div class="wet-row">
+            <div class="wet-col-xs-6 wet-visible-sm wet-visible-xs wet-tofpg">
+              <a href="#wb-cont">Top of Page <span class="glyphicon glyphicon-chevron-up"></span></a>
+            </div>
+            <div class="wet-col-xs-6 wet-col-md-12 wet-text-right">
+              <img src="/boew-wet/wet4.0/GCWeb/assets/wmms-spl.png" alt="" /><span class="wb-inv">Symbol of the
+                Government of Canada</span>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* <div className="bg-light py-3">
-        <div className="container">
-          <div className="row h-100">
-            <div className="col-sm-6">
-            </div>
-            <div className="col-sm-6 canada-logo text-right my-auto">
-              <Image
-                className=""
-                filename="footer-tag.svg"
-                alt={messages.footer_logo_alt}
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </footer>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ import SelectLanguage from './languageSelect';
 import { useIntl } from 'react-intl';
 import { graphql, useStaticQuery } from 'gatsby'
 
+
 const Header = ({homeHeader}) => {
   const intl = useIntl();
   let locale = `${intl.locale}-ca`;
@@ -31,46 +32,107 @@ const Header = ({homeHeader}) => {
   let messages = allPrismicStandaloneestimatorHomepage.nodes.find(function(o){ return o.lang === locale }).data;
 
   return (
-    <header className="mb-5">
-      <div className="container">
-        <div className="row my-3 mx-2">
-          <nav className="skiphold" id="header-skiplink">
-            <a className="sr-only sr-only-focusable aurora-skip skiplink" aria-label="main skiplink" href="#main-content">{messages.header_skip_to_main}</a>
-          </nav>
-          {/* {homeLink === "/en/" &&
-            <div className="align-self-center" property="publisher" typeof="GovernmentOrganization">
-              <Image
-                filename="sig-blk-en.svg"
-                className="goc-logo"
-                alt={messages.header_goc_logo}
-              />
-            </div>
-          }
-          {homeLink === "/fr/" &&
-            <div className="align-self-center" property="publisher" typeof="GovernmentOrganization">
-              <Image
-                filename="sig-blk-fr.svg"
-                className="goc-logo"
-                alt={messages.header_goc_logo}
-              />
-            </div>
-          } */}
-          <section className='text-right align-self-center ml-auto'>
-            <p className="sr-only sr-only-focusable aurora-skip">{messages.header_language_select}</p>
-            <SelectLanguage />
-          </section>
-        </div>
-      </div>
-      <div className="py-4 bg-dark">
-        <div className="container">
-          <div className="row">
-              <div className="col-sm-12">
-                <Link to={homeLink}><h1 className="text-light">{messages.title.text}</h1></Link>
-              </div>
-          </div>
-        </div>
-      </div>
-    </header>
+
+    <header role="banner" class="wet-body">
+		<div id="wb-bnr" class="wet-container">
+			<section id="wb-lng" class="wet-visible-md wet-visible-lg wet-text-right">
+				<h2 class="wb-inv">Language selection</h2>
+				<div class="wet-row">
+					<div class="wet-col-md-12">
+						<ul class="wet-list-inline wet-margin-bottom-none">
+							<li><a lang="fr" href="/cgi-bin/language.pl">Fran&ccedil;ais</a></li>
+						</ul>
+					</div>
+				</div>
+			</section>
+			<div class="wet-row">
+				<div class="wet-brand wet-col-xs-8 wet-col-sm-9 wet-col-md-6"> <a
+						href="http://gcintranet.spac-pspc.gc.ca/gc/index-eng.html"><img
+							src="/boew-wet/wet4.0/theme-tp-pw-gcweb-ca/img/sig-blk-en.svg" alt="" /><span
+							class="wb-inv">Public Services and Procurement Canada</span></a> </div>
+				<section class="wb-mb-links wet-col-xs-4 wet-col-sm-3 wet-visible-sm wet-visible-xs" id="wb-glb-mn">
+					<h2>Search and menus</h2>
+					<ul class="wet-list-inline wet-text-right wet-chvrn">
+						<li><a href="#mb-pnl" title="Search and menus" aria-controls="mb-pnl" class="wet-overlay-lnk"
+								role="button"><span class="glyphicon glyphicon-search"><span
+										class="glyphicon glyphicon-th-list"><span class="wb-inv">Search and
+											menus</span></span></span></a></li>
+					</ul>
+					<div id="mb-pnl"></div>
+				</section>
+				<section id="wb-srch" class="wet-col-xs-6 wet-text-right wet-visible-md wet-visible-lg">
+					<h2>Search</h2>
+					<form action="https://gcintranet-recherche-search.tpsgc-pwgsc.gc.ca/b-eng.php" method="get"
+						name="cse-search-box" role="search" class="wet-form-inline">
+						<div class="wet-form-group">
+							<label for="gcwu-srch" class="wb-inv">Search website</label>
+							<input id="gcwu-srch" class="wb-srch-q wet-form-control" name="q" value="" size="27"
+								maxlength="150" placeholder="Search mySource" type="search" />
+							<input name="checkboxgcintranet" value="true" type="hidden" />
+							<input type="hidden" name="pagesize" value="10" />
+							<input type="hidden" name="page" value="1" />
+							<input type="hidden" name="language" value="en" />
+
+						</div>
+						<div class="wet-form-group submit">
+							<button type="submit" id="wb-srch-sub" class="wet-btn wet-btn-primary wet-btn-small"
+								name="search"><span class="glyphicon-search glyphicon"></span><span
+									class="wb-inv">Search</span></button>
+						</div>
+					</form>
+
+				</section>
+			</div>
+		</div>
+
+		<nav role="navigation" id="wb-sm" data-ajax-replace="/site/wet4.0/html5/includes/sitemenu-eng.html"
+			data-trgt="mb-pnl" class="wb-menu wet-visible-md wet-visible-lg" typeof="SiteNavigationElement">
+			<div class="wet-container nvbar">
+				<h2>Topics menu</h2>
+				<div class="wet-row">
+					<ul class="wet-list-inline wet-menu">
+						<li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/rem-eng.html">Compensation</a></li>
+						<li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/app-proc-eng.html">Procurement</a></li>
+						<li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/immeub-build-eng.html">Buildings and
+								offices</a></li>
+						<li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/fp-gf/index-eng.html">Government finances</a>
+						</li>
+						<li><a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/services-eng.html">More services</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<nav id="wb-bc" class="" property="breadcrumb">
+			<h2 class="wb-inv">You are here:</h2>
+			<div class="wet-container">
+				<div class="wet-row">
+					<ol class="breadcrumb">
+
+						<li> <a href="http://gcintranet.tpsgc-pwgsc.gc.ca/gc/index-eng.html">PSPC GCintranet </a> </li>
+
+						<li><a href="/remuneration-compensation/index-eng.html">GC Travel Guide</a></li>
+
+						<li><a href="/remuneration-compensation/index-eng.html">Travel Calculator</a></li>
+					</ol>
+				</div>
+			</div>
+		</nav>
+	</header>
+
+    // <header className="mb-5">
+    //   <div className="container">
+    //     <div className="row my-3 mx-2">
+    //       <nav className="skiphold" id="header-skiplink">
+    //         <a className="sr-only sr-only-focusable aurora-skip skiplink" aria-label="main skiplink" href="#main-content">{messages.header_skip_to_main}</a>
+    //       </nav>
+    //       <section className='text-right align-self-center ml-auto'>
+    //         <p className="sr-only sr-only-focusable aurora-skip">{messages.header_language_select}</p>
+    //         <SelectLanguage />
+    //       </section>
+    //     </div>
+    //   </div>
+    // </header>
   )
 }
 
