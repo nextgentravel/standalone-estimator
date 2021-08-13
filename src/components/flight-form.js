32cfba18ff68a11de8aeaa5380019e1c31cef802
Mike Jenkins <kinetic@icloud.com>
2021-08-13 17:29:27 -0400

    Changed hour labelling.

--- src/components/flight-form.js
+++ src/components/flight-form.js
@@ -51,11 +51,11 @@ const FlightForm = (props) => {
 
     let times = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
 
-    let hourAbbreviation = props.locale === 'en-ca' ? 'hour' : 'heure';
+    let hourAbbreviation = props.locale === 'en-ca' ? 'hours' : 'heures';
 
     let offsetOptions = [
         {
-            label: `+ / − 1 ${hourAbbreviation}`,
+            label: `+ / − 1 ${props.locale === 'en-ca' ? 'hour' : 'heure'}`,
             value: 1,
         },
         {
