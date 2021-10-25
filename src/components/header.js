a60d3f2b60588e3764f28461aa0b123864c9da4f
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-10-25 11:55:08 -0600

    Made 'Language Selection' an h2

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types"
 import React from "react"
-import Link from 'gatsby-link';
-import SelectLanguage from './languageSelect';
-import { useIntl } from 'react-intl';
-import { graphql, useStaticQuery } from 'gatsby'
-import Image from './image'
+import Link from "gatsby-link"
+import SelectLanguage from "./languageSelect"
+import { useIntl } from "react-intl"
+import { graphql, useStaticQuery } from "gatsby"
+import Image from "./image"
 
-const Header = ({homeHeader}) => {
-  const intl = useIntl();
-  let locale = `${intl.locale}-ca`;
-  let homeLink = `/${intl.locale}/`;
+const Header = ({ homeHeader }) => {
+  const intl = useIntl()
+  let locale = `${intl.locale}-ca`
+  let homeLink = `/${intl.locale}/`
 
   const { allPrismicStandaloneestimatorHomepage } = useStaticQuery(graphql`
     {
@@ -31,7 +31,9 @@ const Header = ({homeHeader}) => {
     }
   `)
 
-  let messages = allPrismicStandaloneestimatorHomepage.nodes.find(function(o){ return o.lang === locale }).data;
+  let messages = allPrismicStandaloneestimatorHomepage.nodes.find(function (o) {
+    return o.lang === locale
+  }).data
 
   return (
     <>
@@ -39,28 +41,44 @@ const Header = ({homeHeader}) => {
         <div className="container">
           <div className="row my-3 mx-2">
             <nav className="skiphold" id="header-skiplink">
-              <a className="sr-only sr-only-focusable aurora-skip skiplink" aria-label="main skiplink" href="#main-content">{messages.header_skip_to_main}</a>
+              <a
+                className="sr-only sr-only-focusable aurora-skip skiplink"
+                aria-label="main skiplink"
+                href="#main-content"
+              >
+                {messages.header_skip_to_main}
+              </a>
             </nav>
-            {homeLink === "/en/" &&
-              <div className="align-self-center" property="publisher" typeof="GovernmentOrganization">
+            {homeLink === "/en/" && (
+              <div
+                className="align-self-center"
+                property="publisher"
+                typeof="GovernmentOrganization"
+              >
                 <Image
                   filename="sig-blk-en.svg"
                   className="goc-logo"
                   alt={messages.header_goc_logo}
                 />
               </div>
-            }
-            {homeLink === "/fr/" &&
-              <div className="align-self-center" property="publisher" typeof="GovernmentOrganization">
+            )}
+            {homeLink === "/fr/" && (
+              <div
+                className="align-self-center"
+                property="publisher"
+                typeof="GovernmentOrganization"
+              >
                 <Image
                   filename="sig-blk-fr.svg"
                   className="goc-logo"
                   alt={messages.header_goc_logo}
                 />
               </div>
-            }
-            <section className='text-right align-self-center ml-auto'>
-              <p className="sr-only sr-only-focusable aurora-skip">{messages.header_language_select}</p>
+            )}
+            <section className="text-right align-self-center ml-auto">
+              <h2 className="sr-only sr-only-focusable aurora-skip">
+                {messages.header_language_select}
+              </h2>
               <SelectLanguage />
             </section>
           </div>
@@ -68,17 +86,23 @@ const Header = ({homeHeader}) => {
         <div className="py-4 header-background mb-3">
           <div className="container">
             <div className="row">
-                <div className="col-sm-12">
-                  <h1 className="text-light">{messages.title.text}</h1>
-                </div>
+              <div className="col-sm-12">
+                <h1 className="text-light">{messages.title.text}</h1>
+              </div>
             </div>
           </div>
         </div>
         <div className="container pl-0">
           <nav aria-label="breadcrumb">
             <ol className="breadcrumb">
-              <li className="breadcrumb-item"><a href={messages.breadcrumb_home_link}>{messages.breadcrumb_home_text}</a></li>
-              <li className="breadcrumb-item active" aria-current="page">{messages.title.text}</li>
+              <li className="breadcrumb-item">
+                <a href={messages.breadcrumb_home_link}>
+                  {messages.breadcrumb_home_text}
+                </a>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                {messages.title.text}
+              </li>
             </ol>
           </nav>
         </div>
