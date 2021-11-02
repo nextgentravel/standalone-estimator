d5651ab14905ce5da6559d051b6003a56024cb3a
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-11-02 15:03:34 -0600

    File now prints to file

--- util/prismicqueryforemail.js
+++ util/prismicqueryforemail.js
@@ -1,9 +1,15 @@
+const fs = require("fs");
 const Prismic = require('@prismicio/client');
 
 const client = Prismic.client("http://gctravelapp.cdn.prismic.io/api")
-return client.query("") // An empty query will return all the documents
+return client.query(Prismic.Predicates.at('document.type', 'standaloneestimator-email-notifications')) // An empty query will return all the documents
   .then(function(response) {
-    console.log("Documents: ", response.results);
+    let json = JSON.stringify(response.results[0].data['standaloneestimator-email-notifications']['message_to_traveller'].value[0].text);
+    console.log("Documents: ", json);
+    fs.writeFile('./email-notifications.json', json, (err) => {
+        if (err) throw err;
+        console.log('JSON email-notifications saved.');
+    });
   }, function(err) {
     console.log("Something went wrong: ", err);
   });
