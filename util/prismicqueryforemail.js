f3bb98cda6973a93b0b2a81161b432ee358c8594
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-11-03 12:01:05 -0600

    Brought in all langauges messages

--- util/prismicqueryforemail.js
+++ util/prismicqueryforemail.js
@@ -6,14 +6,19 @@ const htmlEntities = require('html-entities');
 const client = Prismic.client("http://gctravelapp.cdn.prismic.io/api")
 
 
-return client.query(Prismic.Predicates.at('document.type', 'standaloneestimator-email-notifications')) // An empty query will return all the documents
+return client.query(Prismic.Predicates.at('document.type', 'standaloneestimator-email-notifications'), {lang:'*'})
   .then(function(response) {
-    let result = (PrismicDom.RichText.asHtml(response.results[0].data['standaloneestimator-email-notifications'].message_to_traveller.value))
-    let decoded = (htmlEntities.decode(result))
-    let messages = {
-        toTravellerMessage: decoded
-    }
+      const messages = {}
+    response.results.forEach((item)=>{
+        const lang = (item.lang.slice(0,2));
+        for (const [key, value] of Object.entries(item.data['standaloneestimator-email-notifications'])) {
+            let result = (PrismicDom.RichText.asHtml(value.value))
+            let decoded = (htmlEntities.decode(result))
+            messages[`${key}-${lang}`] = decoded
+          }
+    })
     let data = JSON.stringify(messages);
+    console.log(data)
     fs.writeFile('./api/sendEstimateEmailCeres/prismic-email-notifications.json', data, (err) => {
         if (err) throw err;
         console.log('JSON email-notifications saved.');
