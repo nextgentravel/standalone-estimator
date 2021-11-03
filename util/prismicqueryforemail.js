a2256c29e9acffc98556b75b1523b22c7282b4b3
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-11-03 08:56:14 -0600

    Prints correct text to file

--- util/prismicqueryforemail.js
+++ util/prismicqueryforemail.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
+const PrismicDom = require('prismic-dom')
 const Prismic = require('@prismicio/client');
 
 const client = Prismic.client("http://gctravelapp.cdn.prismic.io/api")
+
+
 return client.query(Prismic.Predicates.at('document.type', 'standaloneestimator-email-notifications')) // An empty query will return all the documents
   .then(function(response) {
-    let json = JSON.stringify(response.results[0].data['standaloneestimator-email-notifications']['message_to_traveller'].value[0].text);
-    console.log("Documents: ", json);
-    fs.writeFile('./email-notifications.json', json, (err) => {
+    // let json = JSON.stringify(response.results[0].data["standaloneestimator-email-notifications"]);
+    let result = (PrismicDom.RichText.asHtml(response.results[0].data['standaloneestimator-email-notifications'].message_to_traveller.value))
+    // console.log("Documents: ", json);
+    fs.writeFile('./email-notifications.json', result, (err) => {
         if (err) throw err;
         console.log('JSON email-notifications saved.');
     });
