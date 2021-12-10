452744bc3d89d7361d39470eb2b3acc7f0411c55
Mitchel Sarauer <Mitchel.Sarauer@tpsgc-pwgsc.gc.ca>
2021-12-10 08:16:21 -0700

    Added more accessibility tests

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -38,7 +38,8 @@ function terminalLog(violations, url) {
 }
 
 describe('accessibility', function () {
-  const siteMap = require('../../sitemap.json')
+  const siteMap = ['en','fr']
+  // change back to require('../../sitemap.json')
   it('has imported sitemap', function () {
     expect(siteMap).to.be.an('array')
   })
@@ -50,5 +51,34 @@ describe('accessibility', function () {
         terminalLog(violations, url)
       })
     });
+
+    it(`\n\n display a load spinner when start is clicked \n\n`, () => {
+      const departDate = Cypress.moment().add(1, 'day').format('YYYY-MM-DD')
+      const returnDate = Cypress.moment().add(5, 'day').format('YYYY-MM-DD')
+
+      cy.get('#origin').type('ottaw{downarrow}{enter}')
+      cy.get('#destination').type('calga{downarrow}{enter}')
+      cy.get('#departureDate').type(`${departDate}`)
+      cy.get('#returnDate').type(`${returnDate}`)
+      cy.get('button').first().click()
+      cy.get('.fa-spin').should('be.visible')
+    });
+
+    it(`\n\n Expanded page should have appropriate accessibility \n\n`, () => {
+      cy.get('#transportation_container', {timeout:15000}).should('be.visible');
+      cy.get("main").injectAxe();
+      cy.checkA11y(null, null, (violations) => {
+        terminalLog(violations, url)
+      })
+    });
+
+    it(`\n\n should pass accessibility tests on flight modal \n\n`, () => {
+      url === 'fr' ? cy.get('select').eq(1).select('Vol') : cy.get('select').eq(1).select('Flight')
+      cy.get('.transportation-message>span').eq(1).children().click()
+      cy.checkA11y(null, null, (violations) => {
+        terminalLog(violations, url)
+      })
+    });
+
   });
 });
