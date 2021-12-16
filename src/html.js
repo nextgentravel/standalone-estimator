31fdbc6d8c3f039d12f7fb7b6813e8eb4a815d2a
Mike Jenkins <kinetic@icloud.com>
2021-12-16 10:39:29 -0500

    Removed meta tag throwing w3 validator error

--- src/html.js
+++ src/html.js
@@ -6,7 +6,6 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-        <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
