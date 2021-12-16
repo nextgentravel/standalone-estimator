fd4bf8769e2a400c68c46df457dec048a19f0de7
Mike Jenkins <kinetic@icloud.com>
2021-12-16 12:30:16 -0500

    Removed commented code.

--- src/components/estimator.js
+++ src/components/estimator.js
@@ -765,52 +765,14 @@ const Estimator = () => {
                 amadeusFlightOffer(originAirportCode, destinationAirportCode, departureDateISODate, returnDateISODate, departureTime, returnTime, departureOffset, returnOffset)
                 .then(response => response.json())
                 .then(result => {
-                    // let date = DateTime.local().toFormat("yyyy-MM-dd");
-                    // let time = DateTime.local().toFormat("hh:mm a")
                     resolve(result);
-                    // if (result.numberOfResults === 0) {
-                    //     localeCopy.flight_no_results.html = localeCopy.flight_no_results.html.replace('{date}', `<strong>${date}</strong>`)    
-                    //     let FlightMessage = <span className="transportation-message alert-warning" role="alert" dangerouslySetInnerHTML={{ __html: localeCopy.flight_no_results.html }}></span>
-                    //     updateTransportationCost(0.00);
-                    //     setTransportationEstimates({
-                    //         ...transportationEstimates,
-                    //         flight: {
-                    //             estimatedValue: 0,
-                    //             estimatedValueMessage: FlightMessage,
-                    //             responseBody: result,
-                    //         }
-                    //     })
-                    //     setTransportationMessage({ element: FlightMessage  })
-                    //     setHaveFlightCost(true);
-                    //     resolve(result);
-                    // } else {        
-                    //     localeCopy.flight_success.html = localeCopy.flight_success.html.replace('{date}', `<strong>${date}</strong>`)
-                    //     localeCopy.flight_success.html = localeCopy.flight_success.html.replace('{time}', `<strong>${time}</strong>`)
-        
-                    //     let FlightMessage = <span className="transportation-message" dangerouslySetInnerHTML={{ __html: localeCopy.flight_success.html }}></span>
-                        
-                    //     setTransportationEstimates({
-                    //         ...transportationEstimates,
-                    //         flight: {
-                    //             estimatedValue: result.flightEstimate,
-                    //             estimatedValueMessage: FlightMessage,
-                    //             responseBody: result,
-                    //         }
-                    //     })
-                    //     setTransportationMessage({ element: FlightMessage  })
-                    //     setHaveFlightCost(true);
-                    //     resolve(result);
-                    // }
                 })
                 .catch(error => {
                     console.log('amadeus flight offer error', error);
-                    // updateTransportationCost(0.00);
-                    // setTransportationMessage({ element: <span className="transportation-message alert-warning" role="alert" dangerouslySetInnerHTML={{ __html: localeCopy.flight_error.html }}></span>  })
                     resolve(error);
                 });
             } else {
                 setLoading(false);
-                // setTransportationMessage({ element: <span className="transportation-message alert-warning" role="alert">{formattedMessage('flight_message_no_airport')}</span>  })
                 resolve('no airport');
             }
         });
