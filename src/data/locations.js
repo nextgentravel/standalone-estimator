fc03174429ae026f2bb1349a464b512c3d806d7b
Mike Jenkins <kinetic@icloud.com>
2022-05-19 09:20:09 -0400

    Updated kilometrics

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,19 +1,19 @@
 module.exports = {
     AB: {
         label: "Alberta",
-        rateCents: "51"
+        rateCents: "51.5"
     },
     BC: {
         label: "British Columbia",
-        rateCents: "56"
+        rateCents: "54"
     },
     MB: {
         label: "Manitoba",
-        rateCents: "52"
+        rateCents: "51.5"
     },
     NB: {
         label: "New Brunswick",
-        rateCents: "54.5"
+        rateCents: "55"
     },
     NL: {
         label: "Newfoundland and Labrador",
@@ -21,7 +21,7 @@ module.exports = {
     },
     NT: {
         label: "Northwest Territories",
-        rateCents: "64.5"
+        rateCents: "62"
     },
     NS: {
         label: "Nova Scotia",
@@ -29,7 +29,7 @@ module.exports = {
     },
     NU: {
         label: "Nunavut",
-        rateCents: "60.5"
+        rateCents: "57"
     },
     ON: {
         label: "Ontario",
@@ -37,18 +37,18 @@ module.exports = {
     },
     PE: {
         label: "Prince Edward Island",
-        rateCents: "53"
+        rateCents: "53.5"
     },
     QC: {
         label: "Quebec",
-        rateCents: "55"
+        rateCents: "55.3"
     },
     SK: {
         label: "Saskatchewan",
-        rateCents: "51.5"
+        rateCents: "50.5"
     },
     YT: {
         label: "Yukon",
-        rateCents: "63"
+        rateCents: "60.5"
     },
 };
